8f7c5f507f0cf06dd774e8bfc9ae2805765933fd
Sofía Molinaro <110236121+Sofia-Molinaro@users.noreply.github.com>
2022-12-25 09:10:32 -0300

    Update Ejercicio14.java

--- Ejercicio14.java
+++ Ejercicio14.java
@@ -1,9 +1,7 @@
 import javax.swing.*;
 public class Ejercicio14 {
     public static void main(String[] args) {
-    String = num1;
-            
-    num1= JOptionPane.showMessageDialog("Ingrese un número, por favor: ");
+    String num1= JOptionPane.showInputDialog("Ingrese un número, por favor: ");
     
     double num2 = Double.parseDouble(num1);
     
