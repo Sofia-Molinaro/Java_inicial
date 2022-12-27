bb8f908909b9a4871a6ddc188605126a8619ee46
Sofía Molinaro <110236121+Sofia-Molinaro@users.noreply.github.com>
2022-12-27 20:35:07 -0300

    Update Ejercicio21.java

--- Ejercicio21.java
+++ Ejercicio21.java
@@ -1,9 +1,29 @@
-
+//Adivina un número generado aleatoriamente por la computadora. 
+import java.util.*;
 public class Ejercicio21 {
+  public static void main(String[] args){
+      int aleatorio = (int)(Math.random()*100);
+      Scanner entrada = new Scanner(System.in);
+      int numero = 0;
+      int intentos = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+      do{
+          
+          intentos ++;
+         
+          System.out.println("Introduce un número, por favor: ");
 
-	}
+          numero = entrada.nextInt();
 
+          if(aleatorio < numero){
+              System.out.print("Es número es menor.");
+          }
+
+          else if(aleatorio > numero){
+              System.out.print("El número es mayor.");
+          }
+      }while(numero!=aleatorio);
+
+      System.out.print("¡Correcto! Lo haz conseguido en " + intentos + " intentos.");
+  }
 }
