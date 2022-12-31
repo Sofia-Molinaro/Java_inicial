e2072baef65c1127542d097128be188bb2c28302
Sofía Molinaro <110236121+Sofia-Molinaro@users.noreply.github.com>
2022-12-31 15:42:57 -0300

    Add files via upload

---
+++ Ejercicio26.java
@@ -0,0 +1,18 @@
+public class Ejercicio26 {
+    public static void main(String[] args){
+        int [] matriz = new int[5];
+        
+        matriz[0] = 2;
+        matriz[1] = 23;
+        matriz[2] = 17;
+        matriz[3] = -6;
+        matriz[4] = 24;
+        
+        System.out.println("La matriz es: " );
+        for(int i = 0; i < 5; i++){
+            System.out.println(matriz[i]);
+        }
+        
+    }
+    
+}
